4a397a05d74748555e7b79d71cbbeeb81d62928d
jatura <baitong41@live.com>
2022-02-22 17:13:28 +0700

    travel page add credit pic

--- react-learn/travel-page/src/components/Travel.js
+++ react-learn/travel-page/src/components/Travel.js
@@ -5,7 +5,10 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 export default function Travel(props){
     return (
         <div className="travel-block">
-            <img className="trvel-img" src={props.imageUrl}/>
+            <div className="image">
+                <img className="trvel-img" src={props.imageUrl}/>
+                <p>Photo by <a target="_blank" href={props.artistUrl}>{props.artist}</a> on <a target="_blank" href="https://unsplash.com/">Unsplash</a></p>
+            </div>
             <div className="detail">
                 <div className="location">
                     <h3><FontAwesomeIcon className="icon"  icon={faLocationDot} />{props.location.toUpperCase()}</h3>
--- react-learn/travel-page/src/data.js
+++ react-learn/travel-page/src/data.js
@@ -4,20 +4,26 @@ export default [
         location: "Greece",
         googleMapsUrl: "https://goo.gl/maps/5EAnCvAq5cqbZBiR6",
         description: "The white-washed cliff-side settlements, the deep blue roofs, and the infinite vistas of the glittering sea make Santorini one of Greece's most stunning islands.",
-        imageUrl: "https://images.unsplash.com/photo-1571406252241-db0280bd36cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        imageUrl: "https://images.unsplash.com/photo-1571406252241-db0280bd36cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        artistUrl: "https://unsplash.com/@alexazabache",
+        artist: "Alex Azabache"
     },
     {
         title: "Budapest",
         location: "Hungary",
         googleMapsUrl: "https://goo.gl/maps/2U2ugWABhfUit6Pv8",
         description: "Budapest is unquestionably one of Europe's most attractive cities. Beautiful architecture, fantasy castles, world-famous hot springs, cool ruin bars, and delectable cuisine are all on my wish list.",
-        imageUrl: "https://images.unsplash.com/photo-1597046502091-a60300249624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        imageUrl: "https://images.unsplash.com/photo-1597046502091-a60300249624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        artistUrl: "https://unsplash.com/@hungarycameraclub",
+        artist: "HungaryCameraClub"
     },
     {
         title: "Prague",
         location: "Czech Republic",
         googleMapsUrl: "https://goo.gl/maps/1bqQRbcUBEp3RgNh6",
         description: "The historic heart of Prague is a UNESCO World Heritage Site, and strolling through the Old Town's quaint tiny alleyways is a truly unique experience. ",
-        imageUrl: "https://images.unsplash.com/photo-1581200309617-836d0d7f9c04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        imageUrl: "https://images.unsplash.com/photo-1581200309617-836d0d7f9c04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        artistUrl: "https://unsplash.com/@whatyouhide",
+        artist: "Andrea Leopardi"
     }
 ]
